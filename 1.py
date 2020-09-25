2d912c466ee62dd8e8743eb8f7e54dbd8f16d524
ArbabMemon <64311080+ArbabMemon@users.noreply.github.com>
2020-09-26 00:12:55 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -230,7 +230,7 @@ def action():
 				oks.append(c+user+pass1)
 			else:
 				if 'www.facebook.com' in q['error_msg']:
-					print '\x1b[1;92mAFTER(3DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass1+ '-\x1b[1;93m✙x1b[1;95m-\x1b[1;91mARBAB-100✓'
+					print '\x1b[1;92mAFTER(3DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass1+ '-\x1b[1;93m✙x1b[1;91mARBAB-100✓'
 					cps = open('save/checkpoint.txt', 'a')
 					cps.write(k+c+user+'|'+pass1+'\n')
 					cps.close()
